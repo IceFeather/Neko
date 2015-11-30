b336bb17bbe2c0f28650d7351f30d371819b646b
IceFeather <benjamin.marlier@gmail.com>
2015-11-30 13:31:22 +0100

    Spinner status color
    Edit spinner status color to white

--- app/src/main/java/com/icefeather/neko/MainActivity.java
+++ app/src/main/java/com/icefeather/neko/MainActivity.java
@@ -48,12 +48,12 @@ public class MainActivity extends AppCompatActivity
 
 
         Spinner status_spinner = (Spinner) findViewById(R.id.status_spinner);
-// Create an ArrayAdapter using the string array and a default spinner layout
+// Create an ArrayAdapter using the string array and a default spinner_item layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.status_array, android.R.layout.simple_spinner_item);
+                R.array.status_array, R.layout.spinner_item);
 // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
+// Apply the adapter to the spinner_item
         status_spinner.setAdapter(adapter);
 
 
